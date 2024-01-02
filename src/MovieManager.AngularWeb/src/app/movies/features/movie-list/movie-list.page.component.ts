ca91a05e7c98022a2d1d7d54e1654d8dacf46edc
Joel Ferreira <30007769@students.ual.pt>
2024-01-02 17:18:50 +0000

    added more new stuff

--- src/MovieManager.AngularWeb/src/app/movies/features/movie-list/movie-list.page.component.ts
+++ src/MovieManager.AngularWeb/src/app/movies/features/movie-list/movie-list.page.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieService } from '../../data-acess/movie.service';
 import { Observable, map } from 'rxjs';
 import { MovieModel } from 'src/app/shared/ui/models/movie/movie.model';
-
+import { MovieTableComponent } from '../../ui/movie-table/movie-table.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-movie-list.page',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './movie-list.page.component.html'
+  imports: [CommonModule,MovieTableComponent,MatFormFieldModule, MatInputModule,FormsModule,MatButtonModule],
+  templateUrl: './movie-list.page.component.html',
+
 })
-export class MovieListPageComponent {
+export class MovieListPageComponent  {
 
   movies$:Observable<MovieModel[]>
   searchTerm:string="";
@@ -21,7 +26,6 @@ export class MovieListPageComponent {
 
   filter() {
     this.movies$ =this.movies$.pipe(
-    map(data => data.filter(m => m.name===this.  searchTerm || m.description===this.  searchTerm )), );
-
+    map(data => data.filter(m => m.name===this.  searchTerm || m.description===this.  searchTerm )),);
 }
 }
